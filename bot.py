7d8a13d1129cad17df2a4f7d54afe077d5478fd7
xqueezeme <xqueezeme>
2022-03-03 16:03:11 +0100

    test

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 # bot.py
 import os
-import discord
 import json
 import stats
 from dotenv import load_dotenv
+import discord
 from discord.ext import commands, tasks
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
+from dateutil import parser
+import datetime
+import random
 
 ranks = {
     'Supersonic Legend' : { 'color': discord.Colour(0x65186D)}, 
@@ -35,11 +38,14 @@ ranks = {
      }
    
 userFile = 'users.json'
+vipQuoteFile = 'vip-posts.json'
+bannedChannelsFile = 'banned-channels.json'
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 CHANNEL = os.getenv('DISCORD_CHANNEL')
-
+VIP = os.getenv('VIP')
+VIP_MESSAGE_CHANNEL = os.getenv('VIP_MESSAGE_CHANNEL')
 intents = discord.Intents.all()
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix="rl")
 slash = SlashCommand(bot, sync_commands=True)
@@ -54,7 +60,13 @@ def getUsers():
     else:
         users = {}
     return users
-
+def getBannedChannels():
+    if os.path.exists(bannedChannelsFile):
+        f = open(bannedChannelsFile)
+        bannedChannels = json.load(f)
+    else:
+        bannedChannels = []
+    return bannedChannels
 def getAllStats():
     users = getUsers()
     if(len(users) > 0):
@@ -113,6 +125,40 @@ async def processData(stats):
                         await member.remove_roles(rlRole)
 
 
+@slash.slash(name="wwds", description="What would Dire say?", guild_ids=guild_ids, options=[
+    create_option(
+      name="keyword",
+      description="keyword",
+      required=False,
+      option_type=3
+    )
+  ]
+)
+async def _wwds(ctx, keyword = None):
+    vipQuotes = getVipQuotes()
+    if vipQuotes:
+        messages = vipQuotes['messages']
+        if messages:
+            if keyword: 
+                lowerKeyword = keyword.lower()
+                messages = list(filter(lambda message: lowerKeyword in message['content'].lower(), messages))
+            if len(messages) > 0:
+                random_item = random.choice(messages)
+                member = await ctx.guild.fetch_member(int(VIP))
+                embed = discord.Embed(
+                            colour=discord.Colour(0xE5E242),
+                            url=random_item['url'],
+                            title='“' + random_item['content'] + '”'
+                        )
+                embed.set_author(name=member.display_name)
+                await ctx.send(embed=embed)         
+            else:
+                await ctx.send('Nothing found')
+        else:
+            await ctx.send('whoops')
+    else:
+        await ctx.send('whoops')
+
 @slash.slash(name="rl-register", description="Register to have the DireBot read your rocket league rank.", guild_ids=guild_ids, options=[
     create_option(
       name="epic_username",
@@ -159,18 +205,86 @@ def addToUserData(id, epic_username):
     with open(userFile, "w") as outfile:
         outfile.write(jsonStr)
 
-       
+def getVipQuotes():
+    if os.path.exists(vipQuoteFile):
+        f = open(vipQuoteFile)
+        vipQuotes = json.load(f)
+    else:
+        vipQuotes = { 'messages': [], 'lastReadMessage':None }
+
+    return vipQuotes
+
+def parseMessage(message):
+    if message.content:
+        return { 'id' : message.id, 'content': message.content, 'authorid': message.author.id, 'created_at': message.created_at.isoformat(), 'url': message.jump_url, 'channel': message.channel.id}
+    return None
+def sortQuoteBy(quote):
+    return quote['created_at']
+
+async def readFullHistory( vipId):
+    print('Reading for full history')
+    vipQuotes = getVipQuotes()
+    messageList = vipQuotes['messages']
+    limit = 200
+    bannedChannels = getBannedChannels()
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            if not channel.id in bannedChannels:
+                messages = await channel.history(limit=limit).flatten()
+                while (len(messages) > 0):
+                    vipMessages = filter(lambda message: message.author.id == int(vipId), messages)
+                    myMesssages = list(filter(lambda message: message != None, map(lambda message : parseMessage(message), vipMessages)))
+                    messageList.extend(myMesssages)
+                    messages = await channel.history(limit=limit, before=messages[-1].created_at).flatten()
+
+    messageList.sort(key=sortQuoteBy)
+    vipQuotes['messages'] = messageList
+    vipQuotes['lastReadMessage'] = datetime.datetime.now().isoformat()
+
+    jsonStr = json.dumps(vipQuotes, indent=4)
+    with open(vipQuoteFile, "w") as outfile:
+        outfile.write(jsonStr)
+
+
+async def readPartialHistory(vipId):
+    limit = 100
+    vipQuotes = getVipQuotes()
+    messageList = vipQuotes['messages']
+    if vipQuotes.get('lastReadMessage'):
+        lastReadMessage = parser.parse(vipQuotes['lastReadMessage'])
+    else:
+        lastReadMessage = None
+    bannedChannels = getBannedChannels()
+
+    for guild in bot.guilds:
+        for channel in guild.text_channels:
+            if not channel.id in bannedChannels:
+
+                messages = await channel.history(limit=limit, after=lastReadMessage).flatten()
+                while (len(messages) > 0):
+                    vipMessages = filter(lambda message: message.author.id == int(vipId), messages)
+                    myMesssages = list(filter(lambda message: message != None, map(lambda message : parseMessage(message), vipMessages)))
+                    messageList.extend(myMesssages)
+                    lastReadMessage = messages[0].created_at
+                    messages = await channel.history(limit=limit, after=lastReadMessage).flatten()
+
+    messageList.sort(key=sortQuoteBy)
+    vipQuotes['messages'] = messageList
+    vipQuotes['lastReadMessage'] = datetime.datetime.now().isoformat()
+    jsonStr = json.dumps(vipQuotes, indent=4)
+    with open(vipQuoteFile, "w") as outfile:
+        outfile.write(jsonStr)
+
 @bot.event
 async def on_ready():
     for guild in bot.guilds:
         if guild.name == GUILD:
             actualGuild = guild
             break
-
+    
     #for role in guild.roles:
     #    if role.name in ranks.keys():
     #        await role.delete()
-    
     if False:
         for rank in list(ranks.keys()):
             role = findRole(guild.roles, rank)
@@ -185,7 +299,7 @@ async def on_ready():
     )
 
     # just trying to debug here
-
+    
     channels = '\n - '.join([channel.name + '-' + str(channel.id) for channel in guild.text_channels])
 
     members = '\n - '.join([member.name + '-' + str(member.id) for member in guild.members])
@@ -201,9 +315,13 @@ import traceback
 timerDelay = 60*10
 async def my_background_task():
     await bot.wait_until_ready()
-    channel = await bot.fetch_channel(CHANNEL) # replace with channel_id
     lastResultJson = None
     while not bot.is_closed():
+        vipQuotes = getVipQuotes()
+        if vipQuotes != None and vipQuotes.get('lastReadMessage') != None:
+            await readPartialHistory(VIP)
+        else:
+            await readFullHistory(VIP)
         try:
             stats = getAllStats()
             jsonStr = json.dumps(stats, indent=4)
